2fc14cd20abcbc3f73323925756aa0da209b38e7
nil.b <nil.borodulya@juds.com.ua>
2017-05-29 17:47:43 +0300

    edit

--- static/js/store.js
+++ static/js/store.js
@@ -1,6 +1,15 @@
 var Store = {
     init: function() {
-        
+        this.onEditRow();
+    },
+    
+    onEditRow: function() {
+        jQuery('.js-edit-row').on('click', function(e) {
+            e.preventDefault();
+            jQuery.post('/admin/users/', {}, function(data) {
+                 
+            });
+        })
     }
 }
 
